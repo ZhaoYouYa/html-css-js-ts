e6bf4d9e23de6e5a77dbf5c6921cd0a261a31fec
unknown <Administrator@BSY036.boshangyun.com>
2021-04-27 17:41:32 +0800

    dfgg

--- vue/index.js
+++ vue/index.js
@@ -1,22 +1,48 @@
-new Vue(
+var jv = new Vue(
     {
         el: "#StockLocationView",
 
+        data:
+        {
+            startDate:"",
+            endDate:"",
+            WarehouseId:null,
+            ErrMessage:"",
+            IsFormtErr:false
+        },
         methods:
         {
             radClick(value) {
                 console.log(value)
             },
             query() {
-                console.log("dsfgg")
-                axios.get('http://localhost:64925/base_common/demo1/1')
-                    .then(function (response) {
-                        console.log(response);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
+              if(CheckStartDateIsLegal()&&CheckEndDateIsLegal())
+              {
+                GetStockByTimeAndWareHouseID()
+              }
+              
+
+
+                // axios.get('http://localhost:64925/base_common/demo1/1')
+                //     .then(function (response) {
+                //         console.log(response);
+                //     })
+                //     .catch(function (error) {
+                //         console.log(error);
+                //     });
             }
         }
     }
 )
+
+function CheckStartDateIsLegal() {
+    console.log(jv.startDate)
+    return true
+}
+function CheckEndDateIsLegal() {
+    console.log(jv.endDate)
+    return true
+}
+function GetStockByTimeAndWareHouseID() {
+    //∑¢ÀÕhttp«Î«Û
+}
